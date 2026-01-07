ca8f46f44bc24fbe5aa49c6e9af2384636dc6958
Developershaha <shahajikokane9049@gmail.com>
2026-01-07 20:56:06 +0530

    what is bean

--- src/main/java/org/Dream/Main.java
+++ src/main/java/org/Dream/Main.java
@@ -4,6 +4,13 @@ package org.Dream;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        // what is bean in spring spring
+
+      /*  Bean is nothing but java object and we can crate the java object usign new keyword and memoery allocated for that object in heap oky
+      *
+      * and any object and lifecycle of that object handle by spring is know as Bean
+      * */
         System.out.println("hello world");
         }
     }
